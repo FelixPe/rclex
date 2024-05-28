6f2713d2f592e810686a0689ab417a9845cfc6c7
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-05-28 14:51:04 +0900

    Treat uint8 array as binary

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,7 @@ config :rclex,
   ros2_message_types: [
     "std_msgs/msg/Empty",
     "std_msgs/msg/String",
+    "std_msgs/msg/UInt8MultiArray",
     "std_msgs/msg/UInt32MultiArray",
     "geometry_msgs/msg/Twist",
     "sensor_msgs/msg/PointCloud",
