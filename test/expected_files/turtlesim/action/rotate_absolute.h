8b8ed332462cf1e41c3b1ad09f74b3682c5ea5c9
Felix Penzlin <eeefix@gmail.com>
2025-05-31 11:29:19 +0200

    remove turtlesim expected files from test

--- test/expected_files/turtlesim/action/rotate_absolute.h
+++
@@ -1,5 +0,0 @@
-#include <erl_nif.h>
-
-// clang-format off
-ERL_NIF_TERM nif_turtlesim_action_rotate_absolute_type_support(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
-// clang-format on
--- test/expected_files/turtlesim/action/rotate_absolute__send_goal__response.h
+++
@@ -1,9 +0,0 @@
-#include <erl_nif.h>
-
-// clang-format off
-ERL_NIF_TERM nif_turtlesim_action_rotate_absolute__send_goal__response_type_support(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
-ERL_NIF_TERM nif_turtlesim_action_rotate_absolute__send_goal__response_create(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
-ERL_NIF_TERM nif_turtlesim_action_rotate_absolute__send_goal__response_destroy(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
-ERL_NIF_TERM nif_turtlesim_action_rotate_absolute__send_goal__response_set(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
-ERL_NIF_TERM nif_turtlesim_action_rotate_absolute__send_goal__response_get(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
-// clang-format on
