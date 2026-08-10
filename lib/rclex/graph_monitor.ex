55ce232c1bc301915d8fc75e418327f2c8a5012c
Felix Penzlin <eeefix@gmail.com>
2026-08-10 08:51:54 +0200

    fix credo

--- lib/rclex/graph_monitor.ex
+++ lib/rclex/graph_monitor.ex
@@ -147,66 +147,79 @@ defmodule Rclex.GraphMonitor do
 
   defp emit_telemetry(diff) do
     if Code.ensure_loaded?(:telemetry) do
-      for {n, ns} <- diff.nodes_joined,
-          do:
-            :telemetry.execute([:rclex, :graph, :node_joined], %{count: 1}, %{
-              node_name: n,
-              node_namespace: ns
-            })
-
-      for {n, ns} <- diff.nodes_left,
-          do:
-            :telemetry.execute([:rclex, :graph, :node_left], %{count: 1}, %{
-              node_name: n,
-              node_namespace: ns
-            })
-
-      for {n, t} <- diff.topics_joined,
-          do:
-            :telemetry.execute([:rclex, :graph, :topic_joined], %{count: 1}, %{
-              topic_name: n,
-              topic_types: t
-            })
-
-      for {n, t} <- diff.topics_left,
-          do:
-            :telemetry.execute([:rclex, :graph, :topic_left], %{count: 1}, %{
-              topic_name: n,
-              topic_types: t
-            })
-
-      for {n, t} <- diff.services_joined,
-          do:
-            :telemetry.execute([:rclex, :graph, :service_joined], %{count: 1}, %{
-              service_name: n,
-              service_types: t
-            })
-
-      for {n, t} <- diff.services_left,
-          do:
-            :telemetry.execute([:rclex, :graph, :service_left], %{count: 1}, %{
-              service_name: n,
-              service_types: t
-            })
-
-      for {n, t} <- diff.actions_joined,
-          do:
-            :telemetry.execute([:rclex, :graph, :action_joined], %{count: 1}, %{
-              action_name: n,
-              action_types: t
-            })
-
-      for {n, t} <- diff.actions_left,
-          do:
-            :telemetry.execute([:rclex, :graph, :action_left], %{count: 1}, %{
-              action_name: n,
-              action_types: t
-            })
+      emit_node_telemetry(diff)
+      emit_topic_telemetry(diff)
+      emit_service_telemetry(diff)
+      emit_action_telemetry(diff)
     else
       Logger.warning(
         "Rclex.GraphMonitor: telemetry not available; add {:telemetry, \"~> 1.0\"} to your deps"
       )
     end
+  end
+
+  defp emit_node_telemetry(diff) do
+    for {n, ns} <- diff.nodes_joined,
+        do:
+          :telemetry.execute([:rclex, :graph, :node_joined], %{count: 1}, %{
+            node_name: n,
+            node_namespace: ns
+          })
+
+    for {n, ns} <- diff.nodes_left,
+        do:
+          :telemetry.execute([:rclex, :graph, :node_left], %{count: 1}, %{
+            node_name: n,
+            node_namespace: ns
+          })
+  end
+
+  defp emit_topic_telemetry(diff) do
+    for {n, t} <- diff.topics_joined,
+        do:
+          :telemetry.execute([:rclex, :graph, :topic_joined], %{count: 1}, %{
+            topic_name: n,
+            topic_types: t
+          })
+
+    for {n, t} <- diff.topics_left,
+        do:
+          :telemetry.execute([:rclex, :graph, :topic_left], %{count: 1}, %{
+            topic_name: n,
+            topic_types: t
+          })
+  end
+
+  defp emit_service_telemetry(diff) do
+    for {n, t} <- diff.services_joined,
+        do:
+          :telemetry.execute([:rclex, :graph, :service_joined], %{count: 1}, %{
+            service_name: n,
+            service_types: t
+          })
+
+    for {n, t} <- diff.services_left,
+        do:
+          :telemetry.execute([:rclex, :graph, :service_left], %{count: 1}, %{
+            service_name: n,
+            service_types: t
+          })
+  end
+
+  defp emit_action_telemetry(diff) do
+    for {n, t} <- diff.actions_joined,
+        do:
+          :telemetry.execute([:rclex, :graph, :action_joined], %{count: 1}, %{
+            action_name: n,
+            action_types: t
+          })
+
+    for {n, t} <- diff.actions_left,
+        do:
+          :telemetry.execute([:rclex, :graph, :action_left], %{count: 1}, %{
+            action_name: n,
+            action_types: t
+          })
   end
 
   defp entity_present?({:node, node_name, node_namespace}, snapshot),
